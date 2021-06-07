da583ef9a253924ff37d897830d44899e38c8de8
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-07 18:51:05 +0530

    Update solution_4.java

--- solution_4.java
+++ solution_4.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class Ques4 {
+public class solution_4 {
 
     public static void main(String args[])
        {
-        Scanner sc = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("Enter a number");
-        int n = sc.nextInt();
+        int n = in.nextInt();
         int n_squ = n*n;  
  
         String str_n = Integer.toString(n);  
