4ca57fc0da37fb4db89fe15bc58e0c852826b8f3
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-07 18:50:31 +0530

    Update solution_5.java

--- solution_5.java
+++ solution_5.java
@@ -1,10 +1,10 @@
-public class Ques5 {
+public class solution_5 {
 	static int inversion(int []a) {
 		int count=0;             
 		
 		for (int i = 0; i < a.length; i++) {
 			for (int j =i+1; j < a.length; j++) {
-				if (i<j &&a[i]>a[j] ) {
+				if (i<j && a[i]>a[j] ) {
 					count++;
 				}
 			}
