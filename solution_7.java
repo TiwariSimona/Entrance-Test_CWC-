e78bfc5efd3eec9d49e35f70531850bb67c350dd
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-07 15:12:25 +0530

    Create solution_7.java

---
+++ solution_7.java
@@ -0,0 +1,14 @@
+public class Ques7{
+
+	
+	public static void main(String[] args) {
+		int a=3;    
+		int d=5;
+		int nthterm=78;
+		int i=((78-3)/5)+1;
+		
+		System.out.println("the value of i is "+i+"  so output is 78");
+		
+	}
+
+}
