661ea4334ea82a0f5587d45a0816a333f416fa43
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-07 18:41:56 +0530

    Update solution_10.java

--- solution_10.java
+++ solution_10.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-public class Ques10 {
+
+public class solution_10 {
  public static void main(String[] args) {
-	  Scanner sc = new Scanner(System.in);
-  
-	  int n;
-  System.out.print("Enter a number ");
-  n = sc .nextInt();
+	  Scanner in = new Scanner(System.in);
+           int n;
+	 
+  System.out.print("Enter  number ");
+  n = in .nextInt();
   int result = n + (n*n) + (n*n*n);
   System.out.printf("The value of n+nn+nnn is " + result);
  }
