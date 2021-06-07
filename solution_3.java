7df58ef57fb6ecd9b8db28f0bc414ad93b3fde56
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-07 18:43:56 +0530

    Update solution_3.java

--- solution_3.java
+++ solution_3.java
@@ -5,11 +5,11 @@ public class solution_3 {
 		Scanner in= new Scanner(System.in);
 		System.out.println("Enter the value of n");
 		int n=in.nextInt();
-		  int a = 0, b = 1, c = 1, d = 2, i;
+		  int a = 0, b = 1, c = 1, d = 2;
 		  int e=0;
 		  System.out.print(a + " " + b + " " + c+" "+d);
 		  
-		  for (i = 4; i <= n; i++) {
+		  for (int i = 4; i <= n; i++) {
 		   e =  b +c+d;
 		   System.out.print(" " + e);
 		   b = c;
