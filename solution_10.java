1dd07c8c709a86ad02af012c14ef971c47a0d426
Simona Tiwari <79264319+TiwariSimona@users.noreply.github.com>
2021-06-08 09:45:52 +0530

    Update solution_10.java

--- solution_10.java
+++ solution_10.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class solution_10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Input number: ");
+		System.out.print("enter number: ");
 		String x=sc.nextLine();
 		System.out.println("value of "+ x + " + " + x + x + " + " + x + x + x + " " +"is :" +" "+(Integer.parseInt(x) + Integer.parseInt(x + x) + Integer.parseInt(x + x + x)));			 
 	}
